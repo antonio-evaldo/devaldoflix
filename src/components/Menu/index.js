588bc10b5c5bb4d426ca193eee49e834f4feb8c4
Antônio Evaldo <antonio.evaldo349@gmail.com>
2020-07-27 19:29:35 -0300

    Atualizando projeto

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import Logo from '../../assets/img/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
 function Menu() {
-    return (
-        <nav className="Menu">
-            <a href="/">    {/* Home */}
-                <img className="Logo" src={Logo} alt="Aluraflix logo" />
-            </a>
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </a>
 
-            <Button as="a" href="/" className="ButtonLink">
-                Prop especifica chamada children
-            </Button>
-
-        </nav>
-    );
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
