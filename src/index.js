32cae3f4edaa6914efe3fd531963c8ad351e65c5
Antônio Evaldo <antonio.evaldo349@gmail.com>
2020-07-28 12:06:13 -0300

    Commit da aula 02

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Video/Categoria';
+
+// Forma mais "correta"
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  //   <Home />
+  // </React.StrictMode>,
+
+  <BrowserRouter>
+    <Switch>
+      {/* Se colocar atributo 'exact', será mais restrito ao caminho especificado. */}
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      {/* <Route component={() => (<div>Página 404</div>)}  /> */}
+      <Route component={Pagina404}  />
+    </Switch>
+  </BrowserRouter>,  
+
   document.getElementById('root')
 );
